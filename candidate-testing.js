1224b152989ba220fe92ef29005e32fc28a1c247
Amanda <amandafaithdev@gmail.com>
2024-05-20 21:55:24 -0500

    Candidate Testing Part 2: Multiple Questions

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,20 +11,25 @@ let candidateAnswer = '';
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "]
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("Enter your name")
+  candidateName = input.question("Enter your name ")
   
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question)
+  // CODE FROM PART 1 candidateAnswer = input.question(question)
+  
+  for (let i = 0; i < questions.length; i++) {
+    let response = input.question(questions[i])
+    candidateAnswers.push(response)
+  }
 
 
 }
@@ -32,11 +37,15 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer === correctAnswer || candidateAnswer === correctAnswer.toLowerCase()) {
-    console.log('Your answer is correct!')
-  } else {
-    console.log('Your answer is incorrect')
-  }
+
+  //CODE FROM PART 1
+  // if (candidateAnswers === correctAnswers) {
+  //   console.log('Your answer is correct!')
+  // } else {
+  //   console.log('Your answer is incorrect')
+  // }
+
+  (`Your answers were ${candidateAnswers}, and the correct answers were ${correctAnswers}`)
 
 
 
@@ -45,6 +54,7 @@ function gradeQuiz(candidateAnswers) {
 
   return grade;
 }
+
 
 function runProgram() {
   askForName();
